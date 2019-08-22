ce52cdd1ccb0467cb339358229a9de1f498b8ba8
Dinesh Thirumaran <dineshthiru.1998@gmail.com>
2019-08-22 23:39:22 +0530

    server updated on consequence with browser

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/browser'));
+app.use(express.static(__dirname + '/browser/browser'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/browser/index.html'));
+res.sendFile(path.join(__dirname+'/browser/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
